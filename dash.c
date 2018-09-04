7ee1c270b933d8c80ccadfd8c28a6613264d4476
Angeera Naser <axn150030@utdallas.edu>
2018-09-04 00:39:19 -0500

    latest

--- dash.c
+++ dash.c
@@ -1,103 +1,129 @@
 /* Angeera Naser
    CS 4348.003 -- Operating Systems
-   25 August 2018 */
+   25 August 2018
 
-/* Create a shell in C.
+   Create a shell in C.
    Do not implement built-in commands.
    Implement commands "exit", "cd", and "path".
-   Implement "interactive" and "batch" modes. */
-
-/* 1: Run program (no extra arguments = interactive, otherwise batch). 
-   2a: If interactive, enter while loop, write prompt, take in input line.
-   2b: If batch, open file, read in input line.
-   3: Determine whether input function is built-in, implemented, or neither.
-   4a: If built-in, access folder. Fork, run execvp process.
-   4b: If implemented, run relevant method.
-   4c: If neither, return error message.
-   5: Repeat for all other lines of input supplied.
-   6: Exit(1) upon exit command.
-*/
+   Implement "interactive" and "batch" modes.
+   Implement redirection.
+   Implement parallel processes. */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <linux/limits.h>
 
-void error() { // write error message
-
+void error() {
   printf("Something has gone horribly wrong.\n");
-
 }
 
-void exec_builtin(char line[]) {}
+void execute_impl (int command, char* arg[][5]) {
+  int i;
+  int arg_num = -1;
 
-void parse_arg(char line[]) {
-
-  const char *impl[3];
-  char result[2][20];
-  char *str1, *str2, *token, *subtoken, *saveptr1, *saveptr2;
-  char args[2][100];
-  int i, j, k;
-
-  impl[0] = "exit";
-  impl[1] = "cd";
-  impl[2] = "path";
-
-  for (j = 1, str1 = line; ; j++, str1 = NULL) {
-    token = strtok_r(str1, "&|", &saveptr1);
-    if (token == NULL) { break; }
-    printf("%d: %s\n", j, token);
-    
-    for (i = 0, str2 = token; ; i++, str2 = NULL) {
-      subtoken = strtok_r(str2, " ", &saveptr2);
-      if (i > 1) { break; }
-      if (subtoken == NULL) { break; }
-      strcpy(args[i], subtoken);
-      printf(" -- %d:  %s\n", i, args[i]);
+  for (i=0; i<5; i++) {
+    if (arg[0][i] != '\0') {
+      arg_num++;
     }
-
-    for (k = 0; k < 3; k++) {
-      if (strcmp(impl[k], token) == 0) { 
-	switch(k) {
-	  case 0:
-	    exit(0);
-	    break;
-	  case 1:
-	    chdir(args[1]);
-	    break;
-	  case 2:
-	    printf("Change path to: %s\n", args[1]);
-	    break;
-	default:
-	    printf("None of these.\n");
-	}
-      	strcpy(result[0], "Implementable!");
-       	strcpy(result[1], args[1]);
-	break;
-      } else {
-	strcpy(result[0], "Nonimplementable.");
-	strcpy(result[1], args[1]);
-      }
-    }
-    printf("%s\n", result[0]);
-    printf("Arguments: %s\n", result[1]);
   }
-  
+
+  printf("Number of arguments: %d\n", arg_num);
+
+  switch(command) {
+    case 0:
+      if (arg_num < 0 || arg_num > 0) { 
+	error(); 
+      } else {
+	exit(0);
+      }
+      break;
+    case 1:
+      if (arg_num < 1 || arg_num > 1) {
+	error();
+      } else {
+	printf("Change directory to: %s\n", arg[0][1]);
+      }
+      break;
+    case 2:
+      printf("Change path to: %s\n", arg[0][1]);
+  }
+
 }
 
-void interactive_mode() {
+void parse_arg(char* command) {
 
-  printf("I am in interactive mode!\n");
-  char *line = NULL;
+  int i, j, k;
+  int implemented = 0;
+  char *str1, *str2, *token, *subtoken, *saveptr1, *saveptr2;
+  char *commands[20][5];
+  const char *impl[3] = {"exit", "cd", "path"};
+
+  for (i=0; i<20; i++) {
+    for (j=0; j<5; j++) {
+	commands[i][j] = '\0';
+      }
+  }
+
+  for (i = 0, str1 = command; ; i++, str1 = NULL) {
+    token, subtoken, saveptr1, saveptr2 = NULL;
+    implemented = 0;
+    token = strtok_r(str1, "&", &saveptr1);
+    if (token == NULL) { break; }
+    commands[i][0] = token;
+    printf("%d: %s\n", i, token);
+
+    for (j = 0, str2 = token; ; j++, str2 = NULL) {
+      subtoken = strtok_r(str2, " ", &saveptr2);
+      if (subtoken == NULL) { break; }
+      commands[i][j] = subtoken;
+      printf(" -- %d: %s\n", j, subtoken);
+    }
+  }
+
+  for (k=0; k<3; k++) {
+    if (strcmp(impl[k], commands[0][0]) == 0) {
+      execute_impl(k, commands);
+      implemented = 1;
+      break;
+    }
+  }
+
+
+
+  //execute_builtin(commands);
+  printf("Implemented: %d\n", implemented);
+
+  for (i=0; i<20; i++) {
+    for (j=0; j<5; j++) {
+      printf("%s ", commands[i][j]);
+    }
+    printf("\n");
+  }
+
+}
+
+int interactive_mode() {
+
+  printf("Interactive mode.\n");
+  char *command = NULL;
   size_t size;
 
   while(1) {
-    line = NULL;
+    command = NULL;
     printf("dash> ");
-    getline(&line, &size, stdin);
-    line[strcspn(line, "\n")] = '\0';
-    parse_arg(line);
+    getline(&command, &size, stdin);
+    command[strcspn(command, "\n")] = '\0';
+    printf("Your command was: %s\n", command);
+    parse_arg(command);
   }
+
+}
+
+int batch_mode(char* command) {
+
+  printf("Batch mode. Argument is: %s\n", command);
 
 }
 
@@ -106,7 +132,7 @@ int main (int argc, char **argv) {
   if (argc == 1) {
     interactive_mode();
   } else if (argc == 2) {
-    printf("I am in batch mode! Your argument is: %s\n", argv[1]);    
+    batch_mode(argv[1]);
   } else {
     error();
   }
